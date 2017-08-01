961c39c17a7cc34d35377b1b861d06e168116c29
Hack Reactor Student <student@hackreactor.com>
2017-08-01 13:22:15 -0700

    setup webpack stuff

---
+++ client/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        The Best REPL Ever Created
+      </div>
+    );
+  }
+}
